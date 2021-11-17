6761d746bf94225dd28840d89133d3edaaa85630
victor.laureano <up201700135@fe.up.pt>
2021-11-17 16:52:15 +0000

    adding first function bignumbers finally

---
+++ src/BigNumbers.hs
@@ -0,0 +1,10 @@
+type BigNumbers = [Int]
+
+-- instance Show BigNumbers where
+ --   show (BigNumbers n) = show (n)
+
+scann :: String  -> BigNumbers
+scann n = reverse (map(`mod` 10) $ reverse $ takeWhile (> 0) $ iterate (`div`10) (read n::Int))
+-- digits n = map (\x -> read [x] :: Int) (show n)
+
+output :: BigNumbers -> S
