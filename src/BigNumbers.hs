b36e16665f4e5a556800a737c213ddc2840ba7a6
victor.laureano <up201700135@fe.up.pt>
2021-11-22 10:49:28 +0000

    commit before pull

--- src/BigNumbers.hs
+++ src/BigNumbers.hs
@@ -1,8 +1,9 @@
 type BigNumbers = [Int]
 
+-- data BigNumbers = BigNumbers [Int]
+
 -- instance Show BigNumbers where
  --   show (BigNumbers n) = show (n)
 
 scann :: String  -> BigNumbers
 scann n = map(`mod` 10) $ reverse $ takeWhile (> 0) $ iterate (`div`10) (read n::Int)
--- digits n = map (\x -> read [x] :: Int) (show n)
