38d3327cc20543fc9ea8a3e2b30b7c3ac902952f
victor.laureano <up201700135@fe.up.pt>
2021-11-22 10:59:52 +0000

    added 2.2 output function

--- src/BigNumbers.hs
+++ src/BigNumbers.hs
@@ -7,3 +7,9 @@ type BigNumbers = [Int]
 
 scann :: String  -> BigNumbers
 scann n = map(`mod` 10) $ reverse $ takeWhile (> 0) $ iterate (`div`10) (read n::Int)
+
+dec2int :: [Int] -> Int
+dec2int a = foldl(\x y -> 10 * x + y) 0 a
+
+output :: BigNumbers -> String
+output xs = show (dec2int xs)
