66f96ea29624f8bcfa75a63e9502b4cfcbe6df6b
victor.laureano <up201700135@fe.up.pt>
2021-11-23 21:25:52 +0000

    using output and scann to make soma and mult

--- src/BigNumbers.hs
+++ src/BigNumbers.hs
@@ -14,14 +14,32 @@ dec2int a = foldl(\x y -> 10 * x + y) 0 a
 output :: BigNumbers -> String
 output xs = show (dec2int xs)
 
+auxSoma :: BigNumbers -> BigNumbers -> BigNumbers
+auxSoma xs ys = zipWith (+) xs ys
+
+auxLenght :: BigNumbers -> Int 
+auxLenght xs = length xs
+
+--somaBN :: BigNumbers -> BigNumbers -> BigNumbers
+--somaBN xs ys =  map(\x -> if (mod x 10 >10) then mod x 10 if (mod x 10 == 10) then (mod x 10) +1 ) (zipWith (+) xs ys)
+
 somaBN :: BigNumbers -> BigNumbers -> BigNumbers
-somaBN xs ys = scann (show (sum [x + y | (x,y) <- zip xs ys]))
+somaBN xs ys = scann(show(sum [read x::Int,read y::Int]))
+    where
+    x = output xs
+    y = output ys
 
 subBN :: BigNumbers -> BigNumbers -> BigNumbers
-subBN xs ys = scann (show (sum [x - y | (x,y) <- zip xs ys]))
+subBN xs ys = scann(show(if x > y then x - y else (-1)*(y - x)))
+    where
+    x = read(output xs)::Int
+    y = read(output ys)::Int
 
 multBN :: BigNumbers -> BigNumbers -> BigNumbers
-multBN xs ys = scann (show (sum [x * y | (x,y) <- zip xs ys]))
+multBN xs ys = scann(show(x*y))
+    where
+    x = read(output xs)::Int
+    y = read(output ys)::Int
 
-divBN :: BigNumbers -> BigNumbers -> (BigNumbers, BigNumbers)
-divBN xs ys = (scann (show (sum [mod x y | (x,y) <- zip xs ys])),scann (show (sum [x `div` y | (x,y) <- zip xs ys])))
+-- divBN :: BigNumbers -> BigNumbers -> (BigNumbers, BigNumbers)
+-- divBN xs ys = (scann (show (sum [mod x y | (x,y) <- zip xs ys])),scann (show (sum [x `div` y | (x,y) <- zip xs ys]))
