a5b7f06e4a7048a5ae402e977a0684d6c8282076
Fab <fabiomiguelchenhuang@gmail.com>
2021-11-24 12:07:08 +0000

    somaBN done

--- src/BigNumbers.hs
+++ src/BigNumbers.hs
@@ -5,20 +5,46 @@ type BigNumbers = [Int]
 -- instance Show BigNumbers where
  --   show (BigNumbers n) = show (n)
 
+-- Converte string para BigNumbers
 scann :: String  -> BigNumbers
 scann n = map(`mod` 10) $ reverse $ takeWhile (> 0) $ iterate (`div`10) (read n::Int)
 
+
+-- Converte BigNumbers para numeros inteiros
 dec2int :: [Int] -> Int
 dec2int a = foldl(\x y -> 10 * x + y) 0 a
 
+
+-- Converte BigNumbers para String
 output :: BigNumbers -> String
 output xs = show (dec2int xs)
 
 somaBN :: BigNumbers -> BigNumbers -> BigNumbers
-somaBN xs ys = scann (show (sum [x + y | (x,y) <- zip xs ys]))
+-- somaBN xs ys = scann (show (sum [x + y | (x,y) <- zip xs ys]))
+somaBN bn1 bn2 = revertMakeNeg (reverse (sumWithCarry (reverse (ifNegMakeNeg bn1)) (reverse (ifNegMakeNeg bn2)) 0 []))
+
+makeNeg :: Num a => a -> a
+makeNeg n = -1*n
+
+ifNegMakeNeg :: BigNumbers -> BigNumbers
+ifNegMakeNeg (x:xs) | x < 0 = x : map makeNeg xs
+                    | otherwise = x:xs
+
+revertMakeNeg :: BigNumbers -> BigNumbers
+revertMakeNeg (x:xs) | x < 0 = x : map abs xs
+                    | otherwise = x:xs
+
+sumWithCarry :: BigNumbers-> BigNumbers -> Int -> BigNumbers  -> BigNumbers
+sumWithCarry [] [] carry res | carry ==  0 = res
+                             | otherwise = res ++ [carry]
+sumWithCarry (x1:xs1) (x2:xs2) carry res | (x1 + x2 + carry) < 0 = sumWithCarry xs1 xs2 ((x1 + x2 + carry) `quot` 10) (res ++ [ ((x1 + x2 + carry) `mod` (-10))])
+                                         | otherwise = sumWithCarry xs1 xs2 ((x1 + x2 + carry) `quot` 10) (res ++ [(x1 + x2 + carry) `mod` 10])
+
+
 
 subBN :: BigNumbers -> BigNumbers -> BigNumbers
-subBN xs ys = scann (show (sum [x - y | (x,y) <- zip xs ys]))
+-- subBN xs ys = scann (show (sum [x - y | (x,y) <- zip xs ys]))
+subBN bn1 bn2 =  [x - y| (x,y) <- zip bn1 bn2]
 
 multBN :: BigNumbers -> BigNumbers -> BigNumbers
 multBN xs ys = scann (show (sum [x * y | (x,y) <- zip xs ys]))
