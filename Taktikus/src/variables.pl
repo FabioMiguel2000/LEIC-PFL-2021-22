f7b6218c62a3e0eafaae42df1fa791604f5fbe3d
Fab <fabiomiguelchenhuang@gmail.com>
2022-01-04 18:08:10 +0000

    seperated into files

---
+++ Taktikus/src/variables.pl
@@ -0,0 +1,4 @@
+game_board_size(8).
+ui(black, 9823).
+ui(white, 9817).
+ui(empty, 9634).
---
+++ Taktikus/src/view.pl
@@ -0,0 +1,64 @@
+% This file contains all visual effects of the game
+:- include('variables').
+
+
+% Displays the gameBoard given by <GameState>
+% display_game(+GameState)
+display_game([GameBoard|PlayerTurn]):-
+    write('\n\n'),
+    display_board(GameBoard),
+    write('\n\n'),
+    display_player_turn(PlayerTurn).
+
+% visually outputs on the CLI the current player to make the move
+display_player_turn(PlayerTurn):-
+    write('Current Turn '),
+    put_code(9758),
+    format(' ~w ', PlayerTurn),
+    ui(PlayerTurn, Code),
+    put_code(Code),
+    write('\n').
+
+% visually outputs on the CLI the gameboard
+display_board(GameBoard):-
+    aux_display_board(GameBoard, 1).
+
+% outputs the row numbers (A, B, C, D, E ...)
+display_row_description(CurrentRowNumber, RowNumber):-
+    CurrentRowNumber = RowNumber,
+    write('\n').
+
+display_row_description(CurrentRowNumber, RowNumber):-
+    \+ CurrentRowNumber > RowNumber,
+    CodeNum is CurrentRowNumber + 64,
+    NewCurrentRowNumber is CurrentRowNumber + 1,
+    put_code(CodeNum),
+    write(' '),
+    display_row_description(NewCurrentRowNumber, RowNumber).
+
+% auxiliar function that helps visually outputs on the CLI a the gameboard
+aux_display_board([], RowNumber):-
+    write('    '),
+    display_row_description(1, RowNumber).
+
+aux_display_board([Row|Rest], RowNumber):-
+    write('  '),
+    write(RowNumber),
+    NextRowNumber is RowNumber + 1,
+    write(' '),
+    display_row(Row),
+    aux_display_board(Rest, NextRowNumber).
+
+% visually outputs on the CLI a particular row of a gameboard
+display_row([]):-
+    write('\n').
+
+display_row([Element|Rest]):-
+    display_cell(Element),
+    write(' '),
+    display_row(Rest).
+
+% visually outputs on the CLI the element ui
+display_cell(Element):-
+    ui(Element, ElementCode),
+    put_code(ElementCode).
