bc5e4b2b0ac8fa3a52bd97603fe33b8dc1578717
Fab <fabiomiguelchenhuang@gmail.com>
2022-01-21 11:36:52 +0000

    organized the files and added description for functions

--- Taktikus/src/game.pl
+++ Taktikus/src/game.pl
@@ -1,8 +1,11 @@
 
+
 :- consult('view.pl').
 :- consult('board.pl').
-:- consult('utils.pl').
 :- consult('moves.pl').
+:- consult('capture.pl').
+:- consult('utils.pl').
+
 
 :- use_module(library(between)).
 
@@ -129,134 +132,6 @@ valid_moves([GameBoard|PlayerTurn], ListOfMoves):-
     element_on_board_moves(GameBoard, BoardSize, PlayerTurn, 1, 1, [], ListOfMoves).
     % length(ListOfMoves, Size),
     % write(Size).
-
-
-% change_board_element([[e,w,w,w,w],[e,e,e,e,e],[w,e,e,e,e],[e,e,e,e,e],[b,b,b,b,b]], 1,3,e,R).
-% valid_horizontal_moves_of_piece([2,2], [[e,w,w,w,w],[w,w,w,e,w],[e,e,e,e,e],[w,e,e,e,e],[e,b,b,b,b]],L).
-
-% valid_vertical_moves_of_piece([5,1], [[w,w,w,w,w],[e,e,e,e,e],[e,e,e,e,e],[e,e,e,e,e],[b,b,b,b,b]],L).
-
-% valid_moves([[[white,white,white,white,white,white,white,white],[empty,empty,empty,empty,empty,empty,empty,empty],[empty,empty,empty,empty,empty,empty,empty,empty],[empty,empty,empty,empty,empty,empty,empty,empty],[empty,empty,empty,empty,empty,empty,empty,empty],[empty,empty,empty,empty,empty,empty,empty,empty],[empty,empty,empty,empty,empty,empty,empty,b],[b,b,b,b,b,b,b,empty]]| b], L).
-
-% check_capture(GameBoard, DestRow, DestColumn, PlayerTurn).
-
-% check_capture_row(GameBoardRow, DestRow, PlayerTurn, ColIndex):-
-%     nth1(ColIndex, GameBoardRow, Ele),
-%     verify_capture(PlayerTurn, Ele)
-    
-% verify_capture(PlayerTurn, PlayerTurn). % no cature yet
-
-% verify_capture(PlayerTurn, Ele):-    %There might be a capture
-%     \+ PlayerTurn = Ele,
-%     % check consecutive Opponents pieces until Own Piece Met
-
-
-% check_until_own_piece(PlayerTurn, Elem, ColIndex, CapturePositionsList):-
-%     %Save current position piece into the list
-%     ColIndex2 is ColIndex +1,
-%     check_until_own_piece(PlayerTurn, ColIndex2, CapturePositionsList).
-
-% check_until_own_piece(PlayerTurn, PlayerTurn, ColIndex, CapturePositionsList):-
-%     %remove all pieces from CapturePositionsList.
-
-checkCapture(GameState, Position, ListOfCaptures):-
-    checkMidCapture(GameState, Position, ListOfMidCaptures),        % Finds all captures that happened due to the first condition capture
-    checkSurroundCapture(GameState, Position, ListOfSurroundCaptures), % Finds all captures that happened due to the second condition capture
-    append(ListOfMidCaptures, ListOfSurroundCaptures, ListOfCaptures).
-
-
-removeCapturedPieces(GameBoard, [], GameBoard):-
-    !.
-
-removeCapturedPieces(GameBoard, [[Row,Col]| Rest], NewGameBoard):-
-    change_board_element(GameBoard, Row, Col, empty, TempNewGameBoard),
-    removeCapturedPieces(TempNewGameBoard, Rest, NewGameBoard).
-
-
-checkMidCapture([GameBoard|PlayerTurn], Position, ListOfCaptures):-
-    game_board_size(BoardSize),
-    checkMidCapture(row, GameBoard, Position, PlayerTurn, BoardSize, ListOfCaptures1),
-    checkMidCapture(col, GameBoard, Position, PlayerTurn, BoardSize, ListOfCaptures2),
-    append(ListOfCaptures1, ListOfCaptures2, ListOfCaptures).
-
-
-% check_capture([[[w,b,w,w,b],[b,b,b,w,w],[e,e,e,e,b],[e,e,e,e,e],[b,b,b,b,b]]|w], [2|5], L).
-
-checkMidCapture(row, _, [_|0], _, _, []):-
-    !.
-checkMidCapture(row, _, [_|GameBoardSize], _, GameBoardSize, []):-
-    !.
-checkMidCapture(col, _, [0|_], _, _, []):-
-    !.
-checkMidCapture(col, _, [GameBoardSize|_], _, GameBoardSize, []):-
-    !.
-
-checkMidCapture(row, GameBoard, [PieceRow|PieceCol], PlayerTurn, _, ListOfCaptures):-
-    VerificationCol1 is PieceCol +1,
-    VerificationCol2 is PieceCol -1,
-    aux_checkMidCapture(GameBoard, [PieceRow|VerificationCol1], [PieceRow|VerificationCol2], PlayerTurn, ListOfCaptures).
-
-checkMidCapture(col, GameBoard, [PieceRow|PieceCol], PlayerTurn, _, ListOfCaptures):-
-    VerificationRow1 is PieceRow +1,
-    VerificationRow2 is PieceRow -1,
-    aux_checkMidCapture(GameBoard, [VerificationRow1|PieceCol], [VerificationRow2|PieceCol], PlayerTurn, ListOfCaptures).
-
-
-aux_checkMidCapture(GameBoard, [VerificationRow1|VerificationCol1], [VerificationRow2|VerificationCol2] ,PlayerTurn, ListOfCaptures):-
-    nth1(VerificationRow1, GameBoard, GameBoardRow1),
-    nth1(VerificationCol1, GameBoardRow1, Ele1),
-    \+ Ele1 = empty,
-    \+ Ele1 = PlayerTurn,
-    nth1(VerificationRow2, GameBoard, GameBoardRow2),
-    nth1(VerificationCol2, GameBoardRow2, Ele2),
-    \+ Ele2 = empty,
-    \+ Ele2 = PlayerTurn,
-    !,  % Condition for this type of capture is met, next, remove Ele1 and Ele2 from the board
-    ListOfCaptures = [[VerificationRow1,VerificationCol1], [VerificationRow2,VerificationCol2]].
-    % change_board_element(GameBoard, VerificationRow1, VerificationCol1, empty, TempNewGameBoard),
-    % change_board_element(TempNewGameBoard, VerificationRow2, VerificationCol2, empty, NewGameBoard).
-
-aux_checkMidCapture(_, _, _ ,_, []).
-
-
-% checkSurroundCapture([[[white,white,white,white,white,white,white,white],[empty,empty,empty,empty,empty,empty,empty,black],[empty,white,black,white,black,black,black,white],[empty,empty,empty,empty,empty,empty,empty,empty],[empty,empty,empty,empty,empty,empty,empty,empty],[empty,empty,empty,empty,empty,empty,empty,empty],[empty,empty,empty,empty,empty,empty,empty,b],[b,b,b,b,b,b,b,empty]]| white], [3|4], L).
-checkSurroundCapture([GameBoard|PlayerTurn], PiecePosition, ListOfCaptures):-
-    checkSurroundCapture(right, GameBoard, PiecePosition, PiecePosition, PlayerTurn, [], ListOfCapturesRight),
-    checkSurroundCapture(left, GameBoard, PiecePosition, PiecePosition, PlayerTurn, [], ListOfCapturesLeft),
-    checkSurroundCapture(top, GameBoard, PiecePosition, PiecePosition, PlayerTurn, [], ListOfCapturesTop),
-    checkSurroundCapture(bottom, GameBoard, PiecePosition, PiecePosition, PlayerTurn, [], ListOfCapturesBottom),
-
-    append(ListOfCapturesRight, ListOfCapturesLeft, ListOfCapturesHorizontal),
-    append(ListOfCapturesTop, ListOfCapturesBottom, ListOfCapturesVertical),
-    append(ListOfCapturesHorizontal, ListOfCapturesVertical, ListOfCaptures).
-
-
-checkSurroundCapture(_, _, _,[0|0], _, _,[]):-
-    !.
-
-checkSurroundCapture(DirectionType, GameBoard, [Row|Col],[Row|Col], PlayerTurn, AccList,ListOfCaptures):-
-    !,
-    index_increment_by_direction(DirectionType, Row, Col, RowIndex2, ColIndex2),
-    checkSurroundCapture(DirectionType, GameBoard, [Row|Col], [RowIndex2|ColIndex2], PlayerTurn, AccList,ListOfCaptures).
-
-
-checkSurroundCapture(DirectionType, GameBoard, [Row|Col], [RowIndex|ColIndex], PlayerTurn, AccList,ListOfCaptures):-
-    nth1(RowIndex, GameBoard, GameBoardRow),
-    nth1(ColIndex, GameBoardRow, Ele),
-    \+ Ele = empty,
-    \+ Ele = PlayerTurn,
-    !,
-    append(AccList, [[RowIndex,ColIndex]], AccList2),
-    index_increment_by_direction(DirectionType, RowIndex, ColIndex, RowIndex2, ColIndex2),
-    checkSurroundCapture(DirectionType, GameBoard, [Row|Col], [RowIndex2|ColIndex2], PlayerTurn, AccList2,ListOfCaptures).
-
-checkSurroundCapture(_, GameBoard, _, [RowIndex|ColIndex], PlayerTurn, AccList,AccList):-
-    nth1(RowIndex, GameBoard, GameBoardRow),
-    nth1(ColIndex, GameBoardRow, Ele),
-    Ele = PlayerTurn,
-    !.
-
-checkSurroundCapture(_, _,_, _, _, _,[]). % if there is an empty space in between, then no capture
 
 
 game_over(GameBoard, OtherPlayer):- 
